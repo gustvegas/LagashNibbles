7dba2fb817587ff8db0167c4f6cd51bfbec6b5d3
diegoglagash <diegog.lagash@gmail.com>
2017-07-29 03:42:14 -0300

    Files must be ignored.

--- wnibbles/frontend/tsjs/client/Direction.js
+++
@@ -1,8 +0,0 @@
-export var Direction;
-(function (Direction) {
-    Direction[Direction["Up"] = 1] = "Up";
-    Direction[Direction["Right"] = 2] = "Right";
-    Direction[Direction["Down"] = 3] = "Down";
-    Direction[Direction["Left"] = 4] = "Left";
-})(Direction || (Direction = {}));
-//# sourceMappingURL=Direction.js.map
